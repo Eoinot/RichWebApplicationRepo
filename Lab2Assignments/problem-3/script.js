564f1e8143a0e8156d2beb3fa9bc7a64b2a2a4b4
Eoinot <134057872+CamCodes1@users.noreply.github.com>
2023-10-20 13:13:09 +0100

    finished problem-3

--- Lab2Assignments/problem-3/script.js
+++ Lab2Assignments/problem-3/script.js
@@ -1,13 +1,16 @@
 let array = [];
-function test2(userName){
+function API(userName){
     let divResult = document.getElementById("result");
+    let divRepoList = document.getElementById("repo-list");
     fetch("https://api.github.com/users/" + userName)
     .then((response) => response.json())
     .then((result) => {
         console.log(result)
 
-        document.getElementById("result").innerHTML = `<img src = "${result.avatar_url}"/>
-        <div class="user-details-div">
+        document.getElementById("result").innerHTML = `
+        <div class="user-details-div"> 
+            <p class="titleUser">User Profile</p>
+            <img src = "${result.avatar_url}"/>
             <p class="output"> Name: ${result.name}</p>
             <p class="output"> UserName: ${result.login}</p>
             <p class="output"> Email: ${result.email}</p>
@@ -17,14 +20,37 @@ function test2(userName){
         `
 
     })
-    // array = json
-    // alert(array[0].title);
+
+    let list = document.getElementById("repo-list");
+    fetch("https://api.github.com/users/" + userName +"/repos")
+    .then((response) => response.json())
+    .then((resultRepo) => {
+        console.log(resultRepo)
+        console.log(resultRepo.length)
+
+        let ul = `
+        <p class="titleUserRepos">User Repos</p>
+        <ul>${resultRepo.map(resultRepo => 
+            `<li class="ul-line-row"> Name:   ${resultRepo.name} 
+                <br/>
+                <br/>
+                <br/>
+                  Description:  ${resultRepo.description} 
+                <br/>
+                <br/>
+                <br/>
+            </li>`).join('')}
+             </ul>`;
+
+        list.innerHTML = ul;
+
+    })
     divResult.style.display = "block";
+    divRepoList.style.display = "block";
 }
 
 function getUserDetails(){
     userNameInput = document.getElementById("userName");
     userName = userNameInput.value.trim();
-    results = test2(userName);
-    console.log(results);
+    results = API(userName);
 }
